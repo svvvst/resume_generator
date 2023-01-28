df762d98657faf23d7faa30ac2fc7fd9c8ef12a9
svvvst <aswyst@gmail.com>
2023-01-28 19:33:55 +0000

    Add completed md>html generator.

--- md2mu.js
+++ md2mu.js
@@ -2,54 +2,31 @@ const fs = require('fs');
 const cfg = require('./cfg.json');
 var   showdown = require('showdown');
 
+// dummy err handler
 function eh(err) { if(err) {return console.log(err);} }
-function returnProperty(obj,propertyStr)
-{
-	if (propertyStr.includes('.'))
-	{
-		propertyStr.split('.').forEach( 
-			(propStr)=>
-			{
-				obj = obj[propStr]
-			}
-		);
-	}
-	return obj;
-}
-function myreplace(htmlStr,tagId,propertyStr)
-{
-	//var re = `(<[\w\s\n]+? id=[\"\"\w\s]+>)[\s\n]+?${tagId}[\s\n]+?(</[\w\s\n]+>)`
-	var re = new RegExp(String.raw`(\w+ id\=${tagId}>).*?(</[\w\s]+>)`,'ms');//new RegExp(re);
-
-	console.log(tagId);
-	console.log('matching...');
-	console.log(htmlStr.match(re));
-
-	return htmlStr.replace(re,'$1'+returnProperty(cfg,propertyStr)+'$2');
-}
 
 const   converter = new showdown.Converter();
 var   res  = converter.makeHtml(fs.readFileSync('resume_full').toString());
 var   css  = fs.readFileSync(cfg.style).toString();
-
 var html = fs.readFileSync(cfg.template).toString();
+var cdata = cfg.contact;
 
+// for each property in obj, create new li tag
+var insStr;
+var srhExp = new RegExp(String.raw`(<ul id=contact>.*?)(</ul>)`,'ms');
 
-var replaceList = 
-{	
-	name:"contact.name",
-	phone:"contact.phone",
-	address:"contact.address"
-
+for (key in cdata) 
+{
+	if (key == 'name') continue;
+	insStr = `<li>${cdata[key]}</li>`;
+	html =  html.replace(srhExp,`$1${insStr}$2`);
 }
 
-    for (key in replaceList)
-	{
-		html = myreplace(html,key,replaceList[key]);
-	}
-   
-    html = html.replace('</ content>',res);
-    html = html.replace('</ style>', '<style>\n'+css+'\n</style>');
+srhExp = /(id=name>.*?)(<\/)/ms;
+html = html.replace(srhExp,`$1${cdata.name}$2`);
 
+html = html.replace('</ content>',res);
+html = html.replace('</ style>', '<style>\n'+css+'\n</style>');
+	
 fs.writeFile(cfg.saveas,html,eh);
 
