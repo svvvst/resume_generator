19f14ba8d8e07c736e55f133caf4bcd7f4237579
svvvst <aswyst@gmail.com>
2023-02-11 12:39:07 -0500

    Update with generators.

---
+++ md2mu.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const showdown = require('showdown');
+const cfg = require('./cfg.json');
+
+// dummy err handler
+function eh(err) { if(err) {return console.log(err);} }
+
+const	cdata 		= cfg.contact;
+const   converter 	= new showdown.Converter();
+
+console.log('Importing markdown...');
+const   res  		= converter.makeHtml(fs.readFileSync(cfg.content).toString());
+const   css  		= fs.readFileSync(cfg.style).toString();
+
+var html = fs.readFileSync(cfg.template).toString();
+
+console.log('Formatting html...');
+
+// Header Formatting
+// for each property in obj, create new li tag
+var insStr;
+var srhExp = new RegExp(String.raw`(<ul id=contact>.*?)(</ul>)`,'ms');
+
+for (key in cdata) 
+{
+	if (key == 'name') continue;  // name is not formatted as list item, ignored and used as heading
+	insStr = `<li>${cdata[key]}</li>`;
+	html =  html.replace(srhExp,`$1${insStr}$2`);
+}
+
+// Add Name Header
+srhExp = /(id=name>.*?)(<\/)/ms;
+html = html.replace(srhExp,`$1${cdata.name}$2`);
+
+// Insert converted resume content
+html = html.replace('</ content>',res);
+
+// Insert style
+html = html.replace('</ style>', '<style>\n'+css+'\n</style>'); //`<link rel="stylesheet" href="${cfg.style}" />`); //
+
+console.log("Saving html...");
+fs.writeFile(cfg.saveas,html,eh);
+console.log('html document saved to: ' + cfg.saveas);
+
---
+++ snippets.js
@@ -0,0 +1,28 @@
+// get property from str 
+function returnProperty(obj,propertyStr)
+{
+	if (propertyStr.includes('.'))
+	{
+		propertyStr.split('.').forEach( 
+			(propStr)=>
+			{
+				obj = obj[propStr];
+			}
+		);
+	}
+	return obj;
+}
+
+// tag replacer
+function myreplace(htmlStr,tagId,propertyStr)
+{
+	//var re = `(<[\w\s\n]+? id=[\"\"\w\s]+>)[\s\n]+?${tagId}[\s\n]+?(</[\w\s\n]+>)`
+	var re = new RegExp(String.raw`(\w+ id\=${tagId}>).*?(</[\w\s]+>)`,'ms');//new RegExp(re);
+
+	//console.log(tagId);
+	//console.log('matching...');
+	//console.log(htmlStr.match(re));
+
+	//return htmlStr.replace(re,'$1'+returnProperty(cfg,propertyStr)+'$2');
+	return htmlStr.replace(re,'$1'+propertyStr+'$2');
+}
